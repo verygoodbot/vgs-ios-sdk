2f3500cbee0fb908fe5630f0979aaee361da5960
Mahmoud Abdelkader <mabdelkader@gmail.com>
2016-11-20 20:01:53 -0800

    first commit

---
+++ Project/VaultSDK/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  VaultSDK
+//
+//  Created by William Locke on 5/3/16.
+//  Copyright © 2016 William Locke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic) IBOutlet UITextField *creditCardNumberTextField;
+
+@end
+
