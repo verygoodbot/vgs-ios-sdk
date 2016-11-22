f8b71d5ec31cdce3397ead090d466b0a01440b84
Fang-Pen Lin <bornstub@gmail.com>
2016-11-22 13:58:33 -0800

    Init project

---
+++ VaultSDK/VaultSDK.h
@@ -0,0 +1,19 @@
+//
+//  VaultSDK.h
+//  VaultSDK
+//
+//  Created by Fang-Pen Lin on 11/22/16.
+//  Copyright © 2016 Very Good Security. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VaultSDK.
+FOUNDATION_EXPORT double VaultSDKVersionNumber;
+
+//! Project version string for VaultSDK.
+FOUNDATION_EXPORT const unsigned char VaultSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VaultSDK/PublicHeader.h>
+
+
